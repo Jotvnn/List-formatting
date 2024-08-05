380eb47352337c80bd773f4567acf37cef361c9e
Jötunn <hvrtache@gmail.com>
2024-08-05 16:03:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+elem0 = document.getElementsByTagName('li')[0];
+elem0.style.color = 'blue';
+elem0.innerHTML = "*" + elem0.innerHTML;
+
+elem1 = document.getElementsByTagName('li')[1];
+elem1.style.color = 'blue';
+elem1.innerHTML = "*" + elem1.innerHTML;
+
+elem2 = document.getElementsByTagName('li')[2];
+// elem2.style.color = 'blue';
+elem2.innerHTML = "*" + elem2.innerHTML;
+
+elem3 = document.getElementsByTagName('li')[3];
+elem3.style.color = 'gold';
+elem3.innerHTML = "*" + elem3.innerHTML;
+
+elem4 = document.getElementsByTagName('li')[4];
+elem4.style.color = 'gold';
+elem4.innerHTML = "*" + elem4.innerHTML;
+
+elem5 = document.getElementsByTagName('li')[5];
+elem5.style.color = 'gold';
+elem5.innerHTML = "*" + elem5.innerHTML;
